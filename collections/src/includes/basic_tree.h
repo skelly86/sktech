ed4a8816924112d1d3621bc28d1b4b0e1cf3f409
S. Kelly <s.kelly86@hotmail.com>
2015-12-22 16:43:31 -0600

    Added nested structure branch.  TODO: implement struct

--- collections/src/includes/basic_tree.h
+++ collections/src/includes/basic_tree.h
@@ -10,19 +10,25 @@
 #define BASIC_TREE_H_
 #include"node.cpp"
 namespace sktech {
-	template<class T>
+	template<class T, class K = unsigned int>
 	class basic_tree {
 	public:
 		virtual basic_tree();
-		virtual basic_tree(const basic_tree &otherTree);
+		virtual basic_tree(const basic_tree &otherTree) = 0;
 		virtual ~basic_tree() = 0;
 		const unsigned int size() const;
 		virtual void insert(const T & newVal) = 0;
-		virtual const T &search();
+		virtual const T &search(const K &searchKey) = 0;
 	protected:
+		typedef struct branch<T,K> : public node<T> {
+			branch();
+			branch(const branch &otherBranch);
+			branch &operator=(const branch &otherBranch);
+			K *key;
+		};
 		node<T> *root;
 		unsigned int _size;
 	private:
 	};
 }
-#endif /* INCLUDES_BASIC_TREE_H_ */
+#endif /* BASIC_TREE_H_ */
