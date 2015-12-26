fa7cb35e7b65312cf18cbf0dcfbde615967ac0b7
S. Kelly <s.kelly86@hotmail.com>
2015-12-26 16:46:37 -0600

    some functions implemented.

--- collections/src/itree.cpp
+++ collections/src/itree.cpp
@@ -17,24 +17,56 @@ namespace sktech {
 		baisc_tree<T,K>::basic_tree(value, newKey);
 	}
 	template<class T, class K = unsigned int>
-	itree<T,K>::itree(const itree &otherTree);
+	itree<T,K>::itree(const itree &otherTree) {
+		copy(otherTree.root);
+	}
 	template<class T, class K = unsigned int>
-	itree<T,K>::~itree();
+	itree<T,K>::~itree() {
+		purge();
+	}
 	template<class T, class K = unsigned int>
-	itree &itree<T,K>::operator=(const itree &otherTree);
+	itree &itree<T,K>::operator=(const itree &otherTree) {
+		if(this != *otherTree) {
+			purge();
+			copy(otherTree.root);
+		}
+		return *this;
+	}
 	template<class T, class K = unsigned int>
-	void itree<T,K>::clear();
+	void itree<T,K>::clear() {
+		purge();
+	}
 	template<class T, class K = unsigned int>
-	void itree<T,K>::insert(const T &newVal, const K &newKey);
+	void itree<T,K>::insert(const T &newVal, const K &newKey) {
+		add(newKey, newVal);
+	}
 	template<class T, class K = unsigned int>
-	const T &itree<T,K>::search(const K &searchKey);
+	const T &itree<T,K>::search(const K &searchKey) {
+		return climb(searchKey).val;
+	}
 	template<class T, class K = unsigned int>
-	void itree<T,K>::copy(const branch *oldBranch);
+	void itree<T,K>::copy(const branch *oldBranch) {
+
+	}
 	template<class T, class K = unsigned int>
-	branch &itree<T,K>::climb(const K &_key);
+	branch<T,K> &itree<T,K>::climb(const K &_key) {
+		branch<T,K> *current = root;
+		while(current != NULL)
+			if(_key == current->key)
+				return *current;
+			else if(_key < current->key)
+				current = current->leftP;
+			else
+				current = current->rightP;
+		return current;
+	}
 	template<class T, class K = unsigned int>
-	void itree<T,K>::add(const K &newKey, const T &value);
+	void itree<T,K>::add(const K &newKey, const T &value) {
+
+	}
 	template<class T, class K = unsigned int>
-	void itree<T,K>::purge();
+	void itree<T,K>::purge() {
+
+	}
 }
 #endif
